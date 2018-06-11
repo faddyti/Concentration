82396af6c25076c7c202fadd3e025f65a1696904
JY@Office <faddyti@live.com>
2018-06-11 13:44:04 +0800

    lecture 3 26:32

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -7,15 +7,32 @@
 //
 
 import Foundation
+
 class Concentration
 {
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        }else{
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // 只有一张牌翻开，判断是否可以和它匹配
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            }else{
+                // 没有翻开的牌，或者是翻开了两张牌的情况
+                // 所有的牌都扣下
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     init(numberOfPairsOfCards: Int) {
