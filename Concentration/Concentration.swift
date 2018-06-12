6d342595f349f0fc3f3d7c02be22ccbb8202ba5f
JY@Office <faddyti@live.com>
2018-06-12 13:59:31 +0800

    lecture 3 1:08:13

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,11 +10,33 @@ import Foundation
 
 class Concentration
 {
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get{
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    }else{
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set{
+            // 没有翻开的牌，或者是翻开了两张牌的情况
+            // 所有的牌都扣下
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chosseCard(at:\(index)): not in the cards.")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 // 只有一张牌翻开，判断是否可以和它匹配
@@ -23,19 +45,13 @@ class Concentration
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             }else{
-                // 没有翻开的牌，或者是翻开了两张牌的情况
-                // 所有的牌都扣下
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
     }
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "init(\(numberOfPairsOfCards)): you must have at least 1 pair of cards.")
         for _ in 0...numberOfPairsOfCards{
             let card = Card()
             cards += [card, card]
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,18 +10,22 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    var flipCount = 0 {
+    var numberOfPairsOfCards: Int{
+        return (cardButtons.count + 1) / 2
+    }
+    
+    private var flipCount = 0 {
         didSet{
             flipCountLabel.text = "Flips:\(flipCount)"
         }
     }
     
-    @IBOutlet var cardButtons: [UIButton]!
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private var cardButtons: [UIButton]!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -31,7 +35,7 @@ class ViewController: UIViewController
         }
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -45,15 +49,25 @@ class ViewController: UIViewController
         }
     }
     
-    var emojiChoices = ["🐶","🐱","🐭","🐳","🐿","🐈","🐕","🦀"]
-    var emoji = [Int:String]()
-    func emoji(for card: Card) -> String {
+    private var emojiChoices = ["🐶","🐱","🐭","🐳","🐿","🐈","🐕","🦀"]
+    private var emoji = [Int:String]()
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
         return emoji[card.identifier] ?? "?"
     }
     
 }
 
+extension Int {
+    var arc4random : Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        }else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        }else {
+            return 0
+        }
+    }
+}
